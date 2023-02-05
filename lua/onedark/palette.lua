f906ecb42855712d4d8913cbdbf22749c9492e7c
QuanQuan <millionfor@apache.org>
2023-02-05 22:22:14 +0800

    fix: up

--- lua/onedark/palette.lua
+++ lua/onedark/palette.lua
@@ -2,10 +2,10 @@ return {
 	dark = {
 		black = "#181a1f",
 		bg0 = "#282c34",
-		bg1 = "#31353f",
+		bg1 = "#282C34",
 		bg2 = "#393f4a",
 		bg3 = "#3b3f4c",
-		bg_d = "#21252b",
+		bg_d = "#282C34",
 		bg_blue = "#73b8f1",
 		bg_yellow = "#ebd09c",
 		fg = "#abb2bf",
